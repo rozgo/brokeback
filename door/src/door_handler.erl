9b55b2aed806ddbc7359d0864b9b3b63e11b4819
rozgo <alex@rozgo.com>
2013-12-04 01:13:33 -0600

    - force temp IP

--- door/src/door_handler.erl
+++ door/src/door_handler.erl
@@ -5,13 +5,13 @@
 -export([terminate/3]).
 
 init(_Type, Req, []) ->
-        {ok, Req, undefined}.
+    {ok, Req, undefined}.
 
 handle(Req, State) ->
-        {ok, Req2} = cowboy_req:reply(200, [
-                {<<"content-type">>, <<"text/plain">>}
+    {ok, Req2} = cowboy_req:reply(200, [
+            {<<"content-type">>, <<"text/plain">>}
         ], <<"Hello world 4!">>, Req),
-        {ok, Req2, State}.
+    {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
-        ok.
+    ok.
