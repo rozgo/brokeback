6360f0e8c6b14a8b2319baec7770146ca3979c43
rozgo <alex@rozgo.com>
2013-12-03 22:33:52 -0600

    chat app

--- chat/src/chat_app.erl
+++ chat/src/chat_app.erl
@@ -5,41 +5,9 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-    Handle = spawn(chat, handle_init, []),
-    register(chat, Handle).
+    chat:start(),
     chat_sup:start_link().
 
 stop(_State) ->
     ok.
 
-handle_init() ->
-    ets:new(chans, [named_table]),
-    handle().
-
-handle() ->
-    receive
-        {{pid, Where}, {text, Msg}} ->
-            Parsed = mochijson2:decode(Msg),
-            command({pid, Where}, {msg, Msg}, Parsed),
-            handle()
-    end.
-
-command({pid, _}, {msg, Msg},
-    {struct,[{<<"cmd">>,<<"pub">>},{<<"chan">>,Chan},{<<"who">>,Who},_]}) ->
-    chan_pid(Chan) ! {pub, {who, Who}, {msg, Msg}};
-command({pid, Where}, {msg, _},
-    {struct,[{<<"cmd">>,<<"join">>},{<<"chan">>,Chan},{<<"who">>,Who}]}) ->
-    chan_pid(Chan) ! {join, {who, Who}, {pid, Where}};
-command({pid, _}, {msg, _},
-    {struct,[{<<"cmd">>,<<"leave">>},{<<"chan">>,Chan},{<<"who">>,Who}]}) ->
-    chan_pid(Chan) ! {leave, {who, Who}}.
-
-chan_pid(Chan) ->
-    case ets:lookup(chans, Chan) of
-        [] ->
-            NewPid = spawn(chan, start, [Chan]),
-            ets:insert(chans, {Chan, NewPid}),
-            NewPid;
-        [{_,Pid}] ->
-            Pid
-    end.
