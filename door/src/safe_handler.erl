787e647d5f1e214cbf96d44d553c603ac81dc9d9
rozgo <alex@rozgo.com>
2014-01-07 14:59:20 -0600

    - unicode module support from couched source code

--- door/src/safe_handler.erl
+++ door/src/safe_handler.erl
@@ -9,13 +9,14 @@ init(_Type, Req, []) ->
 
 handle(Req, State) ->
     {ok, Body, Req2} = cowboy_req:body(Req),
-    io:format("BODY: ~s~n", [Body]),
+    % io:format("BODY: ~s~n", [Body]),
     Query = mochijson2:decode(Body),
+    io:format("Query: ~p~n", [Query]),
     {Code, Response} = command(Query),
     {ok, Req3} = cowboy_req:reply(Code,[
         {<<"Access-Control-Allow-Origin">>, <<"*">>},
         {<<"content-type">>, <<"application/json">>}],
-    mochijson2:encode(Response), Req2),
+        mochijson2:encode(Response), Req2),
     {ok, Req3, State}.
 
 terminate(_Reason, _Req, _State) ->
