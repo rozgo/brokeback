e9e0a001ec1424c7230a55f97923a676a053e348
Kubiak <joe@beyondgames.co>
2014-01-16 20:52:56 -0800

    Putting socket in global process.

--- door/src/push.erl
+++ door/src/push.erl
@@ -1,60 +1,44 @@
 -module(push).
--export([sendpush/1]).
+-export([start/0]).
 
-sendpush({struct,[{<<"method">>,<<"put">>},{<<"token">>,Token},{<<"msg">>,Msg}]}) ->
-  io:format("boom: ~p~n", [Token]),
+start() ->
+  io:format("push socket creation...~n"),
   PrivDir = code:priv_dir(door),
-  %io:format("Priv Dir: ~s", [PrivDir]),
   Address = "gateway.sandbox.push.apple.com",
   Port = 2195,
-  io:format("boom2~n"),
   Cert = PrivDir ++ "/push/apns-dev-cert.pem",
-  io:format("boom3~n"),
   Key = PrivDir ++ "/push/apns-dev-key.noenc.pem",  
 
-  %Options = [{cacertfile, CaCert}, {certfile, Cert}, {keyfile, Key}, {mode, binary}],
   Options = [{certfile, Cert}, {keyfile, Key}, {mode, binary}],
-  Timeout = 1000,
-  io:format("boom4~n"),
+  Timeout = 10000,
+  io:format("socket connecting...~n"),
   {ok, Socket} = ssl:connect(Address, Port, Options, Timeout),
+  io:format("connected~n"),
+  handle(Socket).
 
-  io:format("boom5~n"),
+handle(Socket) ->
+  receive
+    {put, {packet, Packet}, {who, Who}} ->
+      io:format("sending push...~n"),
+      ssl:send(Socket, Packet),
+      io:format("sent.~n"),
+      Value = receive
+              {ssl,{sslsocket,new_ssl,_}, Data} ->
+                  io:format("Client received: ~p~n",[Data])
+              after 2000 ->
+                  0
+              end,
 
-  Payload = mochijson2:encode({struct, [{"aps", {struct, [{"alert", Msg}]}}]}),
-  BPayload = erlang:list_to_binary(Payload),
-  io:format("boom6~n"),
-  PayloadLen = erlang:byte_size(BPayload),
+      ResultMsg = if Value =:= 0 -> <<"success">>;
+                      Value =/= 0 -> <<"error">>
+                   end,
 
-  io:format("boom7~n"),
-  %Token = "9aefcf3355f33ac459e8baa65cc6192017cf7674483f7ce4b63ba8edcb5564f1",
-  %Token = "3f01d3c889e3df2c2ca1b9d103f9a2ef277011b377cd827b521097f5820eba0b",
-  BToken = Token,%util:hexstr_to_bin(Token),
+      PushResult = {200, [{"code",Value}, {"result",ResultMsg}]},
 
-  io:format("boom8~n"),
-  BTokenLength = erlang:byte_size(BToken),
-  io:format("boom9~n"),
-  SomeID= 1,
-  {MSeconds,Seconds,_} = erlang:now(),
-  Expiry = MSeconds * 1000000 + Seconds + 3600*1,
-  io:format("boom10~n"),
-  Packet = <<1:8, SomeID:32/big, Expiry:32/big, BTokenLength:16/big, BToken/binary, PayloadLen:16/big, BPayload/binary>>,
+      Who ! {pushresult, PushResult},
+      handle(Socket);
+    _ ->
+      handle(Socket)
 
-  io:format("boom11~n"),
-  ssl:send(Socket, Packet),
-  io:format("boom12~n"),
-  Value = receive
-          {ssl,{sslsocket,new_ssl,_}, Data} ->
-              io:format("Client received: ~p~n",[Data])
-          after 2000 ->
-              0
-          end,
-  ssl:close(Socket),
-
-  io:format("boom13~n"),
-
-  PushResult = if Value =:= 0 -> "success";
-                  Value =/= 0 -> "error"
-               end,
-
-  {200, [{"code",Value}, {"result",PushResult}]}.
+  end.
 
--- door/src/push_handler.erl
+++ door/src/push_handler.erl
@@ -12,7 +12,11 @@ handle(Req, State) ->
     % io:format("BODY: ~s~n", [Body]),
     Query = mochijson2:decode(Body),
     io:format("Query: ~p~n", [Query]),
-    {Code, Response} = push:sendpush(Query),
+    Packet = createpush(Query),
+    io:format("Created push.~n"),
+    PushResult = sendpush(Packet),
+
+    {Code, Response} = {200, [PushResult]},
     {ok, Req3} = cowboy_req:reply(Code,[
         {<<"Access-Control-Allow-Origin">>, <<"*">>},
         {<<"content-type">>, <<"application/json">>}],
@@ -21,3 +25,34 @@ handle(Req, State) ->
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+createpush({struct,[{<<"method">>,<<"put">>},{<<"token">>,Token},{<<"msg">>,Msg}]}) ->
+  io:format("boom: ~p~n", [Token]),
+  Payload = mochijson2:encode({struct, [{"aps", {struct, [{"alert", Msg}]}}]}),
+  BPayload = erlang:list_to_binary(Payload),
+  PayloadLen = erlang:byte_size(BPayload),
+  BToken = Token,%util:hexstr_to_bin(Token),
+  BTokenLength = erlang:byte_size(BToken),
+  SomeID= 1,
+  {MSeconds,Seconds,_} = erlang:now(),
+  Expiry = MSeconds * 1000000 + Seconds + 3600*1,
+  Packet = <<1:8, SomeID:32/big, Expiry:32/big, BTokenLength:16/big, BToken/binary, PayloadLen:16/big, BPayload/binary>>,
+  Packet.
+
+sendpush(Packet) ->
+    push_pid() ! {put, {packet, Packet}, {who, self()}},
+    receive 
+        {pushresult, PushResult} -> PushResult
+    after
+        10000 -> {200, [{"result",<<"unknown">>}]}
+    end.
+
+
+push_pid() ->
+    case global:whereis_name({push, "push"}) of
+        undefined ->
+            NewPid = spawn(push, start, []),
+            global:register_name({push, "push"}, NewPid),
+            NewPid;
+        Pid -> Pid
+    end.
