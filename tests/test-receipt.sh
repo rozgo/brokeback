00a5d01af460b223bab4f75ea7bd9d4bb3549e8f
Matt Rudder <matt@mattrudder.com>
2014-01-31 15:58:00 -0600

    Modifying test receipt for testing failures.

--- tests/test-receipt.sh
+++ tests/test-receipt.sh
@@ -2,13 +2,13 @@
 
 SCRIPT_PATH=`dirname $0`
 
-echo "Testing valid receipt data..."
+echo "curl -i -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X POST --data-binary \"@$SCRIPT_PATH/sample-receipt-verification.json\" http://localhost:9000/r"
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data-binary "@$SCRIPT_PATH/sample-receipt-verification.json" http://localhost:9000/r
 
 echo
 echo
 
-echo "Testing invalid receipt data..."
+echo "curl -i -H \"Accept: application/json\" -H \"Content-Type: application/json\" -X POST --data-binary \"@$SCRIPT_PATH/sample-receipt-failure.json\" http://localhost:9000/r"
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data-binary "@$SCRIPT_PATH/sample-receipt-failure.json" http://localhost:9000/r
 
 echo
