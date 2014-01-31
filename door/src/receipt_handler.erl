65494bbdf44e0dea6f327bf1499cb5778ac882db
Matt Rudder <matt@mattrudder.com>
2014-01-31 15:41:17 -0600

    Changed receipt verification to respond with error codes for invalid receipt data.

--- door/src/receipt_handler.erl
+++ door/src/receipt_handler.erl
@@ -8,14 +8,14 @@ apple_receipt_url() -> "https://buy.itunes.apple.com/verifyReceipt".
 apple_sandbox_receipt_url() -> "https://sandbox.itunes.apple.com/verifyReceipt".
 
 receipt_status_codes() -> [
-  { 21000, <<"The App Store could not read the JSON object you provided.">> },
-  { 21002, <<"The data in the receipt-data property was malformed.">> },
-  { 21003, <<"The receipt could not be authenticated.">> },
-  { 21004, <<"The shared secret you provided does not match the shared secret on file for your account.">> },
-  { 21005, <<"The receipt server is not currently available.">> },
-  { 21006, <<"This receipt is valid but the subscription has expired.">> },
-  { 21007, <<"This receipt is a sandbox receipt, but it was sent to the production service for verification.">> },
-  { 21008, <<"This receipt is a production receipt, but it was sent to the sandbox service for verification.">> }
+  { 21000, 400, <<"The App Store could not read the JSON object you provided.">> },
+  { 21002, 400, <<"The data in the receipt-data property was malformed.">> },
+  { 21003, 500, <<"The receipt could not be authenticated.">> },
+  { 21004, 500, <<"The shared secret you provided does not match the shared secret on file for your account.">> },
+  { 21005, 503, <<"The receipt server is not currently available.">> },
+  { 21006, 500, <<"This receipt is valid but the subscription has expired.">> },
+  { 21007, 400, <<"This receipt is a sandbox receipt, but it was sent to the production service for verification.">> },
+  { 21008, 400, <<"This receipt is a production receipt, but it was sent to the sandbox service for verification.">> }
 ].
 
 init(_Type, Req, []) ->
@@ -23,7 +23,6 @@ init(_Type, Req, []) ->
 
 handle(Req, State) ->
     {ok, Body, Req2} = cowboy_req:body(Req),
-    io:format("Received validation request: ~p~n", [Body]),
     VerifyRes = send_receipt_request(mochijson2:decode(Body)),
     
     {Code, Result} = process_receipt_response(VerifyRes),
@@ -52,24 +51,24 @@ get_receipt_status(Req) ->
 
 send_receipt_request({struct, [{<<"receipt-data">>, Receipt}]}) ->
     Json = binary:list_to_bin(mochijson2:encode({struct, [{<<"receipt-data">>, Receipt}]})),
-    io:format("Sending request to ~s: ~p~n", [apple_receipt_url(), Json]),
+    io:format("Sending request to ~s~n", [apple_receipt_url()]),
     Req = create_receipt_request(apple_receipt_url(), Json),
     Status = get_receipt_status(Req),
 
-    io:format("Got response: ~p~n", [Status]),
     case Status of
       21007 ->
         io:format("Sandbox receipt receieved, redirecting to ~s~n", [apple_sandbox_receipt_url()]),
 
         RedirReq = create_receipt_request(apple_sandbox_receipt_url(), Json),
-        io:format("Got response: ~p~n", [get_receipt_status(RedirReq)]),
+        io:format("Apple receipt verification responded with: ~p~n", [get_receipt_status(RedirReq)]),
 
         RedirReq;
       _ ->
+        io:format("Apple receipt verification responded with: ~p~n", [Status]),
         Req
     end;
 send_receipt_request(_) ->
-  {error, "invalid receipt-data"}.
+  {client_error, "invalid receipt-data"}.
 
 % should we return an HTTP error code with invalid receipt verifications?
 process_receipt_response({ok, Result}) ->
@@ -80,19 +79,24 @@ process_receipt_response({ok, Result}) ->
 
     ValidReceipt = { <<"valid_receipt">>, Code == 0 },
     StatusDesc = lists:keyfind(Code, 1, receipt_status_codes()),
-    Response = case Code of
+    {ResponseCode, Response} = case Code of
       0 ->
-        [ValidReceipt];
+        {200, [ValidReceipt]};
       _ when is_tuple(StatusDesc) ->
-        {_, Desc} = StatusDesc,
-        [ValidReceipt, { <<"error">>, Desc }]
+        {_, HttpCode, Desc} = StatusDesc,
+        io:format("Error response from Apple: ~s~n", [Desc]),
+        {HttpCode, [ValidReceipt]}
     end,
 
-    {200, mochijson2:encode({struct, Response})};
+    {ResponseCode, mochijson2:encode({struct, Response})};
 process_receipt_response({error, Reason}) ->
-    {200, mochijson2:encode({struct, [
-      { <<"valid_receipt">>, false },
-      { <<"error">>, Reason }
+    io:format("HTTP client error during request to Apple: ~s~n", [Reason]),
+    {500, mochijson2:encode({struct, [
+      { <<"valid_receipt">>, false }
+    ]})};
+process_receipt_response({client_error, _}) ->
+    {400, mochijson2:encode({struct, [
+      { <<"valid_receipt">>, false }
     ]})}.
 
 terminate(_Reason, _Req, _State) ->
--- tests/test-receipt.sh
+++ tests/test-receipt.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
 SCRIPT_PATH=`dirname $0`
+
+echo "Testing valid receipt data..."
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data-binary "@$SCRIPT_PATH/sample-receipt-verification.json" http://localhost:9000/r
+
+echo
+echo
+
+echo "Testing invalid receipt data..."
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data-binary "@$SCRIPT_PATH/sample-receipt-failure.json" http://localhost:9000/r
 
 echo
